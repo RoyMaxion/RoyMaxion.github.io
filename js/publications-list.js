4cb83444d0328b0ab23b27273c1bd933c7815822
Roy Maxion <roy.maxion@gmail.com>
2021-02-21 02:11:10 -0500

    Initial Commit

---
+++ js/publications-list.js
@@ -0,0 +1,178 @@
+/*
+ * Modify the list below to change your list of publications.
+ */
+
+var publicationList = [
+    {
+        keywords: 'Steganography, Anomaly detection',
+        title: 'Detecting Steganography in Images from Mobile Stego Apps Using Random Statistical Properties.',
+        // link: '(too new, not on the internet yet)',
+        citation:
+            'Pierre, S.; Lin, L.; Chen, W.; Martin, A.; Guan, Y.; Maxion, R. and Newman, J. “Detecting Steganography in Images from Mobile Stego Apps Using Random Statistical Properties.” International Conference on Forensic and Investigative Science (ICFIS), 15-16 February 2021.London, England.',
+        date: 'February 2021',
+        publisher: 'IEEE Security & Privacy',
+        // citedBy: '(too new, no one wrote a paper yet)',
+    },
+    {
+        keywords: 'Biometrics, Rhythm, Data collection, Protocols, Computer security, Measurement',
+        title: 'Reproducibility: Buy Low, Sell High',
+        link: 'https://ieeexplore.ieee.org/document/9204790',
+        citation:
+            'Maxion, Roy.  Reproducibility: Buy Low, Sell High. IEEE Security & Privacy, Vol. 18, No. 6, pp. 33-41, November 2020.',
+        date: 'November 2020',
+        publisher: 'IEEE Security & Privacy',
+        // citedBy: '(too new, no one wrote a paper yet)',
+    },
+    {
+        keywords: 'Mouse dynamics, Authentication, Feature extraction, Monitoring',
+        title: 'Pattern-Growth based Mining Mouse-Interaction Behavior for an Active User Authentication System',
+        link: 'https://ieeexplore.ieee.org/document/8100881',
+        citation:
+            'Shen, Chao; Chen, Yufei; Guan, Xiaohong and Maxion, Roy. "Pattern-Growth based Mining Mouse-Interaction Behavior for an Active User Authentication System."IEEE Transactions on Dependable and Secure Computing, Vol. 17, No. 2, pp. 335-349, March 2020.',
+        date: 'March 2020',
+        publisher: 'IEEE Transactions on Dependable and Secure Computing',
+        citedBy: 'https://scholar.google.com/scholar?cites=18349029192075291804&as_sdt=5,39&sciodt=0,39&hl=en',
+    },
+    {
+        keywords: 'Stress measurement, Monitoring, Biomedical monitoring',
+        title: 'Distinction of Stress and Non-Stress Tasks using Facial Action Units',
+        link: 'https://dl.acm.org/doi/abs/10.1145/3281151.3281158',
+        citation:
+            'C. Viegas, S. Lau, R. Maxion and A. Hauptmann."Distinction of Stress and Non-Stress Tasks using Facial Action Units."Proceedings of the 20th International Conference on Multimodal Interaction, Article No. 7, Boulder, Colorado, 16-20 October 2018, ACM, New York, NY.',
+        date: 'October 2018',
+        publisher: "ICMI '18",
+        // citedBy: '(too new, no one wrote a paper yet)',
+    },
+    {
+        keywords: 'Stress, Lips, Stress measurement, Moniotring, Biomedical monitoring, Data visualization',
+        title: 'Towards Independent Stress Detection: a Dependent Model Using Facial Action Units',
+        link: 'https://ieeexplore.ieee.org/document/8516497',
+        citation:
+            'C. Viegas, S. Lau, R. Maxion and A. Hauptmann."Towards Independent Stress Detection: a Dependent Model Using Facial Action Units." IEEE Signal Processing Society, International Conference on Content-Based Multimedia Indexing (CBMI), pp. 1-6, 4-6 September 2018, La Rochelle, France.',
+        date: 'September 2018',
+        publisher: 'IEEE Signal Processing Society, CBMI',
+        citedBy: 'https://scholar.google.com/scholar?cites=8593972691384889391&as_sdt=5,39&sciodt=0,39&hl=en',
+    },
+
+    {
+        keywords: 'Stress, Lips, Stress measurement, Moniotring, Biomedical monitoring, Data visualization',
+        title: 'Application of HAZOP to the Design of Cyber Security Experiments',
+        link: 'https://ieeexplore.ieee.org/document/7474170',
+        citation:
+            'Mansoori, Masood; Welch, Ian; Choo, Kim-Kwang Raymond and Maxion, Roy. “Application of HAZOP to the Design of Cyber Security Experiments.”IEEE International Conference on Advanced Information Networking and Applications (AINA-2016), Le Régent Congress Centre, Crans-Montana, Switzerland, March 23-25, 2016.',
+        date: 'March 2016',
+        publisher: 'IEEE, AINA-2016',
+        citedBy: 'https://scholar.google.com/scholar?cites=10218766869671740591&as_sdt=5,39&sciodt=0,39&hl=en',
+    },
+    {
+        keywords: 'Keystroke dynamics, rhythms',
+        title: 'Clusters and Markers for Keystroke Typing Rhythms',
+        link: 'https://www.usenix.org/system/files/conference/laser2014/laser-2014-paper-lau.pdf',
+        citation:
+            '.Lau, Shing-hon and Maxion, Roy A.“Clusters and Markers for Keystroke Typing Rhythms."In Proceedings of the 3rd Annual Workshop on Learning from Authoritative Security Experiment Results (LASER 2014), Arlington, Virginia, 15-16 October 2014, ACM Press, New York, NY, 2014.',
+        date: 'October 2014',
+        publisher: 'LASER 2014',
+        citedBy: 'https://scholar.google.com/scholar?cites=18433182455823873927&as_sdt=5,39&sciodt=0,39&hl=en',
+    },
+    {
+        keywords: 'Biometrics, Mouse dynamics, Authentication, Eigenspace transformation, One-class learning',
+        title: 'User Authentication Through Mouse Dynamics',
+        link: 'https://ieeexplore.ieee.org/document/6327662',
+        citation:
+            'Shen, Chao; Cai, Zhongmin; Guan, Xiaohong; Du, Youtian and Maxion, Roy A. "User Authentication through Mouse Dynamics" IEEE Transactions on Information Forensics and Security, Vol. 8, No. 1, pp. 16-30, January 2013.',
+        date: 'January 2013',
+        publisher: 'IEEE Transactions on Information Forensics and Security',
+        citedBy: 'https://scholar.google.com/scholar?cites=15349214904781050549&as_sdt=5,39&sciodt=0,39&hl=en',
+    },
+    {
+        keywords: 'Mouse dynamics, Support vector machine classification, Classification algorithms',
+        title: 'Comparing classification algorithm for mouse dynamics based user identification',
+        link: 'https://ieeexplore.ieee.org/document/6327662',
+        citation:
+            'Shen, Chao; Cai, Zhongmin; Maxion, Roy A.; Xiang, Guang and Guan, Xiaohong. "Comparing Classification Algorithms for Mouse Dynamics based User Identification." IEEE 5th International Conference on Biometrics: Theory, Applications and Systems (BTAS 2012), 23-26 September 2012, Washington, DC, pp. 61-66, IEEE Press, Piscataway, New Jersey.',
+        date: 'September 2012',
+        publisher: 'BTAS 2012',
+        citedBy: 'https://scholar.google.com/scholar?cites=14823405669986798103&as_sdt=5,39&sciodt=0,39&hl=en',
+    },
+    {
+        keywords: 'Keystroke dynamics',
+        title: 'Free vs. transcribed text for keystroke-dynamics evaluations',
+        link: 'https://dl.acm.org/doi/10.1145/2379616.2379617',
+        citation:
+            'Killourhy, Kevin S. and Maxion, Roy A. "Free vs. Transcribed Text for Keystroke-Dynamics Evaluations." LASER-12: Proceedings of the 2012 Workshop: Learning from Authoritative Security Experiment Results, pp. 1-8, 18-19 July 2012, Arlington, VA. ACM, New York, New York.',
+        date: 'July 2012',
+        publisher: 'LASER-12',
+        citedBy: 'https://scholar.google.com/scholar?cites=2156536760471052336&as_sdt=5,39&sciodt=0,39&hl=en',
+    },
+    {
+        keywords: 'Experimentation, Measurement, Operational definitions, Validity',
+        title: 'Making experiments dependable',
+        link: 'https://www.cs.cmu.edu/~maxion/pubs/Maxion12.pdf',
+        citation:
+            'Killourhy, Kevin S. and Maxion, Roy A. "Free vs. Transcribed Text for Keystroke-Dynamics Evaluations." LASER-12: Proceedings of the 2012 Workshop: Learning from Authoritative Security Experiment Results, pp. 1-8, 18-19 July 2012, Arlington, VA. ACM, New York, New York.',
+        date: '2011',
+        publisher: 'Springer, Berlin, Heidelberg',
+        citedBy: 'https://scholar.google.com/scholar?cites=10925824603872210709&as_sdt=5,39&sciodt=0,39&hl=en',
+    },
+    {
+        keywords: 'Anomaly detection, Keystroke dynamics, Experimental methodology',
+        title: 'Why Did My Detector Do That?! Predicting Keystroke-Dynamics Error Rates.',
+        link: 'https://www.cs.cmu.edu/~maxion/pubs/KillourhyMaxion10.pdf',
+        citation:
+            'Killourhy, Kevin S. and Maxion, Roy A. "Why Did My Detector Do That?! Predicting Keystroke-Dynamics Error Rates." 13th International Symposium on Recent Advances in Intrusion Detection, 15-17 September 2010, Ottawa, Ontario, Canada. S. Jha, R. Sommer, and C. Kreibich (Eds.): RAID 2010, Lecture Notes in Computer Science, LNCS Vol. 6307, pp. 256-276, Springer-Verlag, Berlin/Heidelberg, 2010. ISBN 978-3-642-15511-6.',
+        date: 'September 2010',
+        publisher: 'RAID 2010',
+        citedBy: 'https://scholar.google.com/scholar?cites=15314731644598665712&as_sdt=5,39&sciodt=0,39&hl=en',
+    },
+    {
+        keywords: 'Biometrics, Keystroke dynamics, Authentication, Telegraphy',
+        title: 'Keystroke Biometrics with Number-Pad Input',
+        link: 'https://www.cs.cmu.edu/~maxion/pubs/MaxionKillourhy10a.pdf',
+        citation:
+            'Maxion, Roy A. and Killourhy, Kevin S. "Keystroke Biometrics with Number-Pad Input." In IEEE/IFIP International Conference on Dependable Systems & Networks (DSN-10), pp. 201-210, Chicago, Illinois, 28 June to 01 July 2010. IEEE Computer Society Press, Los Alamitos, California, 2010.',
+        date: '2010',
+        publisher: 'IEEE Computer Society Press',
+        citedBy: 'https://scholar.google.com/scholar?cites=2361092647544153643&as_sdt=5,39&sciodt=0,39&hl=en',
+    },
+    {
+        keywords: 'Keystroke dynamics',
+        title: 'Comparing Anomaly-Detection Algorithms for Keystroke Dynamics',
+        link: 'https://www.cs.cmu.edu/~maxion/pubs/KillourhyMaxion09.pdf',
+        citation:
+            'Killourhy, Kevin S. and Maxion, Roy A. "Comparing Anomaly-Detection Algorithms for Keystroke Dynamics." In International Conference on Dependable Systems & Networks (DSN-09), pp. 125-134, Estoril, Lisbon, Portugal, 29 June to 02 July 2009. IEEE Computer Society Press, Los Alamitos, California, 2009.',
+        date: '2009',
+        publisher: 'IEEE Computer Society Press',
+        citedBy: 'https://scholar.google.com/scholar?cites=3911431193134176570&as_sdt=5,39&sciodt=0,39&hl=en',
+        dataSet: 'http://www.cs.cmu.edu/~keystroke',
+    },
+    {
+        keywords: 'Keystroke dynamics',
+        title: 'The Effect of Clock Resolution on Keystroke Dynamics',
+        link: 'https://www.cs.cmu.edu/~maxion/pubs/KillourhyMaxion08a.pdf',
+        citation:
+            'Killourhy, Kevin S. and Maxion, Roy A. "The Effect of Clock Resolution on Keystroke Dynamics." In 11th International Symposium on Recent Advances in Intrusion Detection (RAID-08), 15-17 September 2008, Cambridge, Massachusetts, R. Lippmann, E. Kirda and A. Trachtenberg (Eds.), Lecture Notes in Computer Science (LNCS), Vol. 5230, pp. 331-350, Springer-Verlag, Berlin, Heidelberg.',
+        date: 'September 2008',
+        publisher: 'RAID-08',
+        citedBy: 'https://scholar.google.com/scholar?cites=9239963968023030895&as_sdt=5,39&sciodt=0,39&hl=en',
+    },
+    {
+        keywords: 'Detectors, Data privacy, Computerized monitoring, Postal services',
+        title: 'Toward Realistic and Artifact-Free Insider-Threat Data',
+        link: 'https://www.cs.cmu.edu/~maxion/pubs/KillourhyMaxion07.pdf',
+        citation:
+            'Killourhy, Kevin S. and Maxion, Roy A. "Toward Realistic and Artifact-Free Insider-Threat Data." In 23rd Annual Computer Security Applications Conference (ACSAC-07), pp. 87-96, Miami Beach, Florida, 10-14 December 2007. IEEE Computer Society Press, Los Alamitos, California, 2007.',
+        date: 'December 2007',
+        publisher: 'ACSAC-07',
+        citedBy: 'https://scholar.google.com/scholar?cites=10156996524432846622&as_sdt=5,39&sciodt=0,39&hl=en',
+    },
+    {
+        keywords: 'Writing, Personal digital assistants, Biometrics, Handwriting recognition',
+        title: 'User Discrimination Through Structured Writing on PDAs.',
+        link: 'https://www.cs.cmu.edu/~maxion/pubs/KillourhyMaxion07.pdf',
+        citation:
+            'Roberts, Rachel R. M.; Maxion, Roy A.; Killourhy, Kevin S., and Arshad, Fahd. "User Discrimination Through Structured Writing on PDAs." In International Conference on Dependable Systems & Networks (DSN-07), pp. 378-387, Edinburgh, Scotland, 25-28 June 2007. IEEE Computer Society Press, Los Alamitos, California, 2007.',
+        date: 'June 2007',
+        publisher: 'DSN-07',
+        citedBy: 'https://scholar.google.com/scholar?cites=13892723574521503601&as_sdt=5,39&sciodt=0,39&hl=en',
+    },
+];
---
+++ js/publications.js
@@ -0,0 +1,118 @@
+/*
+ * this file is a renderer of the publication list.
+ * to modify the list itself, edit js/publications-list.js
+ */
+
+function createPublicationCard(paper) {
+    var cardDOM = document.createElement('div');
+    cardDOM.className = 'card';
+
+    // add keyword
+    if (paper.keywords) {
+        var keywordsDOM = document.createElement('span');
+        keywordsDOM.appendChild(document.createTextNode(paper.keywords));
+        keywordsDOM.className = 'caption';
+        cardDOM.appendChild(keywordsDOM);
+    }
+
+    // add title
+    if (paper.link) {
+        var titleDOM = document.createElement('a');
+        titleDOM.href = paper.link;
+        titleDOM.setAttribute('target', '_blank');
+        var titleTextDOM = document.createElement('h3');
+        titleTextDOM.appendChild(document.createTextNode(paper.title));
+        titleDOM.appendChild(titleTextDOM);
+        cardDOM.appendChild(titleDOM);
+    } else {
+        var titleDOM = document.createElement('h3');
+        titleDOM.className = 'not-available';
+        titleDOM.appendChild(document.createTextNode(paper.title));
+        var titleHintDOM = document.createElement('span');
+        titleHintDOM.className = 'hint-text';
+        titleHintDOM.appendChild(document.createTextNode('(Link not publicized yet)'));
+        titleDOM.appendChild(titleHintDOM);
+        cardDOM.appendChild(titleDOM);
+        cardDOM.setAttribute('title', 'This paper is not available on the internet at the moment.');
+    }
+
+    // add citation
+    if (paper.citation) {
+        var citationDOM = document.createElement('p');
+        citationDOM.appendChild(document.createTextNode(paper.citation));
+        cardDOM.appendChild(citationDOM);
+    }
+
+    // add date published
+    if (paper.date) {
+        var dateDOM = document.createElement('span');
+        dateDOM.className = 'tag';
+        var dateIconDOM = document.createElement('span');
+        dateIconDOM.className = 'material-icons';
+        dateIconDOM.appendChild(document.createTextNode('event'));
+        dateDOM.appendChild(dateIconDOM);
+        dateDOM.appendChild(document.createElement('span')).appendChild(document.createTextNode(paper.date));
+        cardDOM.appendChild(dateDOM);
+    }
+
+    // add publisher
+    if (paper.publisher) {
+        var publisherDOM = document.createElement('span');
+        publisherDOM.className = 'tag';
+        var publisherIconDOM = document.createElement('span');
+        publisherIconDOM.className = 'material-icons';
+        publisherIconDOM.appendChild(document.createTextNode('text_snippet'));
+        publisherDOM.appendChild(publisherIconDOM);
+        publisherDOM.appendChild(document.createElement('span')).appendChild(document.createTextNode(paper.publisher));
+        cardDOM.appendChild(publisherDOM);
+    }
+
+    // add "see who cited this"
+    if (paper.citedBy) {
+        var citedByDOM = document.createElement('a');
+        citedByDOM.className = 'tag';
+        citedByDOM.href = paper.citedBy;
+        citedByDOM.setAttribute('target', '_blank');
+        var citedByIconDOM = document.createElement('span');
+        citedByIconDOM.className = 'material-icons';
+        citedByIconDOM.appendChild(document.createTextNode('format_quote'));
+        citedByDOM.appendChild(citedByIconDOM);
+        citedByDOM
+            .appendChild(document.createElement('span'))
+            .appendChild(document.createTextNode('See who cited this'));
+        cardDOM.appendChild(citedByDOM);
+    }
+
+    // add "view scripts and data set"
+    if (paper.dataSet) {
+        var dataSetDOM = document.createElement('a');
+        dataSetDOM.className = 'tag';
+        dataSetDOM.href = paper.dataSet;
+        dataSetDOM.setAttribute('target', '_blank');
+        var dataSetIconDOM = document.createElement('span');
+        dataSetIconDOM.className = 'material-icons';
+        dataSetIconDOM.appendChild(document.createTextNode('stacked_bar_chart'));
+        dataSetDOM.appendChild(dataSetIconDOM);
+        dataSetDOM
+            .appendChild(document.createElement('span'))
+            .appendChild(document.createTextNode('View data set and scripts'));
+        cardDOM.appendChild(dataSetDOM);
+    }
+
+    return cardDOM;
+}
+
+/*
+ * this function is executed when the webpage finishes loading.
+ */
+(function () {
+    var cardListDOM = document.getElementById('publication-list');
+
+    publicationList.forEach((paper) => {
+        cardListDOM.appendChild(createPublicationCard(paper));
+    });
+
+    // update the publication count
+    var publicationCountDOM = document.getElementById('publication-count');
+    publicationCountDOM.innerHTML = publicationList.length.toString();
+})();
